1dfbbeb4a1815e9701f9a6c6e0ef65fabb0af6d6
gelther <lpw.sormano@gmail.com>
2016-04-10 19:21:14 +0200

    Code consistency changes for src/Util/Rewrites.php

--- src/Util/Rewrites.php
+++ src/Util/Rewrites.php
@@ -8,7 +8,7 @@ class Rewrites {
 	 * Listen to WordPress language changes as this is important for our rewrites
 	 */
 	public function listen_language_change() {
-		add_action( 'update_option_WPLANG', function ( $old_value, $value, $option ) {
+		add_action( 'update_option_WPLANG', function( $old_value, $value, $option ) {
 			$this->mark_flush_needed();
 		}, 10, 3 );
 	}
@@ -22,7 +22,7 @@ class Rewrites {
 	 */
 	public function maybe_flush() {
 		if ( 1 == get_option( 'wpcm_rewrites_need_flush', false ) ) {
-			add_action( 'init', function () {
+			add_action( 'init', function() {
 				flush_rewrite_rules();
 				delete_option( 'wpcm_rewrites_need_flush' );
 			}, 999 );
