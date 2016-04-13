6cbd138df51cfd0f8083dcd9ea76124a16b43f8f
PrintHouseCorp <hello@printhouse.co.uk>
2016-04-13 15:34:44 +0100

    Code consistency changes for src/Helper/Date.php

--- src/Helper/Date.php
+++ src/Helper/Date.php
@@ -4,7 +4,6 @@ namespace Never5\WPCarManager\Helper;
 /**
  * Class Date
  * @package Never5\WPCarManager\Helper
- *
  */
 abstract class Date {
 
@@ -14,8 +13,7 @@ abstract class Date {
 	 * @return String
 	 */
 	public static function get_date_format() {
-		return apply_filters( 'wpcm_date_format', wp_car_manager()->service('settings')->get_option('date_format') );
+		return apply_filters( 'wpcm_date_format', wp_car_manager()->service( 'settings' )->get_option( 'date_format' ) );
 	}
-
 
 }
